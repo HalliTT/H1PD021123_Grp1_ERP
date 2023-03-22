eefc9df9a4ced539f80200d6a8991e0ad62eee1a
sbw01 <sbw01@outlook.dk>
2023-03-22 14:59:08 +0100

    [K2] - 02.04 - Added some files for making screen work. Most of them are being deleted later on

--- Src/Program.cs
+++ Src/Program.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using H1PD021123_Grp1_ERP.Customer;
+using System;
 using TECHCOOL;
+using TECHCOOL.UI;
+using H1PD021123_Grp1_ERP.Screens;
+
 namespace App
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            
+        {      
+            MyFirstScreen firstScreen = new MyFirstScreen();
+            TodoListScreen todo = new TodoListScreen();
+            CustomerListScreen customerList = new CustomerListScreen();
+            Screen.Display(customerList);
         }
     }
 }
---
+++ Src/Screens/CustomerList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1PD021123_Grp1_ERP.Screens
+{
+    class CustomerList
+    {
+        public string CustomerID { get; set; } = "";
+        public string CustomerName { get; set; } = "";
+        public string CustomerPhone { get; set; } = "";
+        public string CustomerMail { get; set; } = "";  
+
+        public CustomerList(string customerId, string customerName, string customerPhone, string customerMail) 
+        {
+            CustomerID = customerId;
+            CustomerName = customerName;
+            CustomerPhone = customerPhone;
+            CustomerMail = customerMail;
+        }
+    }
+}
