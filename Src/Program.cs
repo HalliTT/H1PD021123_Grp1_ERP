0aa8ec25f0200725d1076314ee71e8cb91407411
Niclas Hansen <32816311+UXinFront@users.noreply.github.com>
2023-03-22 10:26:15 +0100

    tilføjet nuget pack

--- Src/Program.cs
+++ Src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TECHCOOL;
 namespace App
 {
     internal class Program
@@ -6,6 +7,7 @@ namespace App
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
         }
     }
 }
