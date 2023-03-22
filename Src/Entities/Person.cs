c7eaebc96471925c49e35646e1cf933314276f16
sbw01 <sbw01@outlook.dk>
2023-03-22 13:31:05 +0100

    [D4] - 02.02 - Added Person entity class and methods

---
+++ Src/Entities/Person.cs
@@ -0,0 +1,44 @@
+﻿using H1PD021123_Grp1_ERP.Customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1PD021123_Grp1_ERP.Entities
+{
+    public class Person
+    {
+        string name;
+        string surname;
+        private Adress adress;
+        private string phoneNumber;
+        
+
+
+        public string getName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string getSurname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+       public string getPhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public string getFullName()
+        {
+            string fullname = name + " " + surname;
+            return fullname;
+            
+        }
+    }
+}
