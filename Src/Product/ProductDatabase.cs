bfc3dcc4cb5e6a52a1db42d70a68bfb502275db7
Niclas Hansen <32816311+UXinFront@users.noreply.github.com>
2023-03-23 09:11:44 +0100

    [D1.P1] added database class for products -> merge

--- Src/Product/ProductDatabase.cs
+++ Src/Product/ProductDatabase.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TECHCOOL;
 
 namespace H1PD021123_Grp1_ERP.Product
 {
     partial class Database
     {
-        
+
+        private static void GetProducts()
+        { 
+            
+        }
     }
 }
