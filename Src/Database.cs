653cc69896ee9157423f9837dd1f63dccf18ca2e
Niclas Hansen <32816311+UXinFront@users.noreply.github.com>
2023-03-24 11:24:34 +0100

    [P1,D2] Added trustcert for connect SQL to db

--- Src/Database.cs
+++ Src/Database.cs
@@ -10,6 +10,8 @@ namespace App
             SqlConnectionStringBuilder sqlBuilder = new();
             sqlBuilder.DataSource = "docker.data.techcollege.dk";
             sqlBuilder.InitialCatalog = "H1PD021123_Gruppe1";
+            sqlBuilder.TrustServerCertificate = true;
+            sqlBuilder.IntegratedSecurity = false;
             sqlBuilder.UserID = "H1PD021123_Gruppe1";
             sqlBuilder.Password = "H1PD021123_Gruppe1";
             this.connectionStr = sqlBuilder.ToString();
