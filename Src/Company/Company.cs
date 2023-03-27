2c464db41b767cead5f7b7ca39221d492629aae2
Niclas Hansen <32816311+UXinFront@users.noreply.github.com>
2023-03-27 13:48:37 +0200

    [V3,V4,V5] added idea/ to gitignore

--- Src/Company/Company.cs
+++ Src/Company/Company.cs
@@ -1,6 +1,6 @@
 using System;
 using TECHCOOL;
-namespace App.Company
+namespace App
 {
     internal class Company
     {
--- Src/Company/CompanyScreen.cs
+++ Src/Company/CompanyScreen.cs
@@ -10,10 +10,10 @@ namespace App
         protected override void Draw()
         {
             Clear(this);
-            ListPage<Company.Company> listCompany = new ListPage<Company.Company>();
-            listCompany.Add(new Company.Company("700", "Mars", "Hovedvejen", "88", "9200", "Nørresundby", "Denmark", "Dkk", "29 22 00 10", "Mars@Mars.dk"));
-            listCompany.AddColumn("Id", "companyId");
-            listCompany.AddColumn("Company",    "companyName");
+            ListPage<Company> listCompany = new ListPage<Company>();
+            listCompany.Add(new Company("700", "Mars", "Hovedvejen", "88", "9200", "Nørresundby", "Denmark", "Dkk", "29 22 00 10", "Mars@Mars.dk"));
+            listCompany.AddColumn($"id", "companyId");
+            listCompany.AddColumn("Company", "companyName");
             listCompany.AddColumn("Road", "companyRoad");
             listCompany.AddColumn("House Number", "companyHouseNumber");
             listCompany.AddColumn("Zip Code", "companyZipCode");
@@ -25,16 +25,16 @@ namespace App
             listCompany.Draw();
         }
     }
-
+    
     public class CompanyShortListScreen : Screen
     {
         public override string Title { get; set; } = "List of Companies";
         protected override void Draw()
         {
             Clear(this);
-            ListPage<Company.Company> listCompany = new ListPage<Company.Company>();
-            listCompany.Add(new Company.Company("200", "Venus", "Landevejen", "2", "9000", "Aalborg", "Denmark", "Dkk", "29 54 90 22", "Venus@venus.dk"));
-            listCompany.Add(new Company.Company("700", "Mars", "Hovedvejen", "88", "9200", "Nørresundby", "Denmark", "Dkk", "29 22 00 10", "Mars@Mars.dk"));
+            ListPage<Company> listCompany = new ListPage<Company>();
+            listCompany.Add(new Company("200", "Venus", "Landevejen", "2", "9000", "Aalborg", "Denmark", "Dkk", "29 54 90 22", "Venus@venus.dk"));
+            listCompany.Add(new Company("700", "Mars", "Hovedvejen", "88", "9200", "Nørresundby", "Denmark", "Dkk", "29 22 00 10", "Mars@Mars.dk"));
             listCompany.AddColumn("Company", "companyName");
             listCompany.AddColumn("Country", "companyCountry");
             listCompany.AddColumn("Currency", "companyCurrency");
