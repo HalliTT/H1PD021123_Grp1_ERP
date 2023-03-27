e017772677928557bf3e109dfee507b2f752c9fd
Haraldur <haraldur@live.dk>
2023-03-27 13:04:26 +0200

    [S2] - 2.1 - Display full Sale list

--- Src/Program.cs
+++ Src/Program.cs
@@ -8,38 +8,20 @@ namespace App
         static void Main(string[] args)
         {
 
-            ///// ------ Haraldur Test ------ /////
-            // var shortScreen = new CompanyShortListScreen();
-            // Screen.Display(shortScreen);
-            // var fullScreen = new CompanyFullListScreen();
-            // Screen.Display(fullScreen);
-
-            ///// ------ DB - TEST ------ /////
-            
-            var product = new Product("12", "test", 1, 10.0, "test", 10, Unit.meters);
-
-            var orderline = new List<OrderLine> { new OrderLine("123", product, 1, "2", "3") };
-
-            var timestamp = DateTime.Now.ToString();
-
-            var order = new Sales(1234, timestamp, timestamp, "12", State.None, orderline, 200);
-
+            ///// ------ Haraldur SalesScreen ------ /////
             var db = new Database();
-
-            db.InsertOrder(order);
-
-            Console.WriteLine("Press enter to get order from db");
-            Console.ReadLine();
-
-            var orders = db.GetOrder(1234);
-
-            foreach (var i in orders)
+            var orders = db.GetOrders();
+            var listSales = new ListPage<Sales>();
+            foreach (var orderI in orders)
             {
-                Console.WriteLine(i.customerNumber);
+                listSales.Add(orderI);
             }
+            var fullListScreen = new SalesFullListScreen(listSales);
+            Screen.Display(fullListScreen);
 
-            db.DeleteOrder(order);
-            
+            // db.DeleteOrder(order);
+            // db.DeleteOrder(orderTwo);
+
             //another impl
             // var products = new Product.Product("", "", 0, 0, 0, "", 0, Product.Unit.meter);
             // products.PrintProducts();
