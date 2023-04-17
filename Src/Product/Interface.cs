b9bbb77a55ce4e57674d120aef6ee7daeb1d722a
Marcus Haukelid Larsen <mhl@rtx.dk>
2023-04-17 09:10:25 +0200

    Done with refactoring for now, need some help from sinb regarding how to get consolekey working

--- Src/Product/Interface.cs
+++ Src/Product/Interface.cs
@@ -1,3 +1,76 @@
+using TECHCOOL.UI;
+
 namespace App
 {
+    public class ProductInterface
+    {
+        public ProductInterface(string title,
+                                ListPage<Product> list, 
+                                ListPage<Product> list_selected, 
+                                Form<Product> editor)
+        {
+            this._title = title;
+            this._list = list;
+            this._list_selected = list_selected;
+            this._editor = editor;
+
+            this._intrface = new Interface<Product> (this._title, 
+                                                     this._list, 
+                                                     this._list_selected, 
+                                                     this._editor);
+        }
+
+        protected Interface<Product> _intrface;
+
+        protected string _title = null!;
+        public string title
+        {
+            get { return _title; }
+        }
+
+        protected ListPage<Product> _list = null!;
+        public ListPage<Product> list
+        {
+            get { return _list; }
+        }
+
+        protected ListPage<Product> _list_selected = null!;
+        public ListPage<Product> list_selected
+        {
+            get { return _list_selected; }
+        }
+        protected Form<Product> _editor = null!;
+        public Form<Product> editor
+        {
+            get { return _editor; }
+        }
+
+        public void Show()
+        {
+            // Preview list interface
+            this._list.AddColumn("Id", "Id");
+            this._list.AddColumn("Name", "name");
+            this._list.AddColumn("Amount", "amountInStock");
+            this._list.AddColumn("Purchase price", "purchasePrice");
+            this._list.AddColumn("Sales price", "salesPrice");
+            this._list.AddColumn("Profit", "profit");
+
+            // Selected element interface
+            this._list_selected.AddColumn("Id", "Id");
+            this._list_selected.AddColumn("Name", "name");
+            this._list_selected.AddColumn("Description", "description");
+            this._list_selected.AddColumn("Sales price", "salesPrice");
+            this._list_selected.AddColumn("Purchase price", "purchasePrice");
+            this._list_selected.AddColumn("Location", "location");
+            this._list_selected.AddColumn("Amount", "amountInStock");
+            this._list_selected.AddColumn("Unit", "unit");
+            this._list_selected.AddColumn("Profit", "percentageProfit");
+            this._list_selected.AddColumn("Profit", "profit");
+
+            // Editor interface
+
+            Screen.Display(this._intrface);
+        } 
+
+    }
 }
--- Src/Program.cs
+++ Src/Program.cs
@@ -9,29 +9,17 @@ namespace App
         {
             var list = new ListPage<Product> {};
 
-            list.AddColumn("Id", "Id");
-            list.AddColumn("Name", "name");
-
             var product = new Product("product", "test", 10, 100, "DK", 2, Unit.meters);
             list.Add(product);
 
             var selected = new ListPage<Product> {};
 
-            selected.AddColumn("Id", "Id");
-            selected.AddColumn("Name", "name");
-            selected.AddColumn("Description", "description");
-            selected.AddColumn("Sales price", "salesPrice");
-            selected.AddColumn("Purchase price", "purchasePrice");
-            selected.AddColumn("Location", "location");
-
             var form = new Form<Product> {};
 
             form.TextBox("Name", "name");
 
-            var screen = new Interface<Product> ("Product list", list, selected, form);
-            Screen.Display(screen);
-
-            screen.Create();
+            var screen = new ProductInterface ("Product list", list, selected, form);
+            screen.Show();
         }
     }
 }
