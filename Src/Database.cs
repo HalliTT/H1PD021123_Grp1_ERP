68f36b9a52d806f4cfb3b6c66ea35376b5ddd202
Niclas Hansen <32816311+UXinFront@users.noreply.github.com>
2023-04-19 13:34:31 +0200

    [S7] check SQL connection added to Database.cs

--- Src/Database.cs
+++ Src/Database.cs
@@ -1,4 +1,6 @@
+using K4os.Compression.LZ4.Encoders;
 using Microsoft.Data.SqlClient;
+using SqlException = System.Data.SqlClient.SqlException;
 
 namespace App
 {
@@ -7,7 +9,8 @@ namespace App
         public Database()
         {
             SqlConnectionStringBuilder sqlBuilder = new();
-            sqlBuilder.DataSource = "docker.data.techcollege.dk";
+            // sqlBuilder.DataSource = "docker.data.techcollege.dk";
+            sqlBuilder.DataSource = "192.168.1.70";
             sqlBuilder.InitialCatalog = "H1PD021123_Gruppe1";
             sqlBuilder.TrustServerCertificate = true;
             sqlBuilder.IntegratedSecurity = false;
@@ -17,9 +20,57 @@ namespace App
             sqlBuilder.IntegratedSecurity = false;
 
             _connection = new SqlConnection(sqlBuilder.ToString());
-            _connection.Open();
+            
+            bool retry = true;
 
+            while (retry)
+            {
+                try
+                {
+                    Console.WriteLine("Connecting to the database...");
+                    // Check the connection state
+                    using (SqlConnection connection = new SqlConnection(sqlBuilder.ToString()));
+                    {
+                        connection.Open();
+                        Console.WriteLine("Connection established successfully.");
+
+                        // Check the connection state
+                        if (connection.State == System.Data.ConnectionState.Open)
+                        {
+                            Console.WriteLine("Connection is open and available.");
+                        }
+                    }
+
+                    retry = false;
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("An error occurred while connecting to the database:");
+                    Console.WriteLine(ex.Message);
+
+                    Console.WriteLine("Do you want to retry the connection? (Y/N)");
+                    string input = Console.ReadLine()?.ToUpperInvariant();
+                    switch (input)
+                    {
+                        case "Y":
+                            retry = true;
+                            break;
+                        case "N":
+                            retry = false;
+                            Console.WriteLine("Quitting...");
+                            Thread.Sleep(3000);
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Output. Try again.");
+                            retry = true;
+                            break;
+                    }
+                }
+            }
         }
+
+
 
         // Destructor
         ~Database() 
@@ -27,6 +78,12 @@ namespace App
             _connection.Close(); 
         }
 
+        protected bool _status;
+        public bool status
+        {
+            get { return _status; }
+        }
+        
         protected SqlConnection _connection = null!;
         public SqlConnection connection
         {
