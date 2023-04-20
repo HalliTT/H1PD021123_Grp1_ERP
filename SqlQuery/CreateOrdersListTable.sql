f59083762bac3348864f37f8c082f7c910ce0816
Haraldur <haraldur@live.dk>
2023-04-20 14:36:13 +0200

    Fix querry

--- SqlQuery/CreateOrdersListTable.sql
+++ SqlQuery/CreateOrdersListTable.sql
@@ -5,5 +5,5 @@ create table OrdersList
 	Id	INT PRIMARY KEY IDENTITY (1,1),
 	OrdersId INT NOT NULL REFERENCES Orders(Id),
 	ProductId INT REFERENCES Products(Id),
-	Amount INT NOT NULL
+	Amount INT
 )
